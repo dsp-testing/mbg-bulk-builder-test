fe824d868a955d8ede89ccab03d122037ac8aecc
Koen Vlaswinkel <koesie10@github.com>
2022-12-05 19:45:56 +0100

    Add Swift

---
+++ Sources/bulk-builder-test/bulk_builder_test.swift
@@ -0,0 +1,8 @@
+@main
+public struct bulk_builder_test_xcode {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(bulk_builder_test_xcode().text)
+    }
+}
