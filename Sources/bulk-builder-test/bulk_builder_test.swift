f6b06566c1175f89af8680f0fea7065c8eb7d2a2
Koen Vlaswinkel <koesie10@github.com>
2022-12-07 09:03:09 +0100

    Use iOS app for testing Swift
    It doesn't seem like a pure Swift project is buildable by CodeQL, so
    this will use an iOS app created using XCode instead.

--- Sources/bulk-builder-test/bulk_builder_test.swift
+++
@@ -1,8 +0,0 @@
-@main
-public struct bulk_builder_test_xcode {
-    public private(set) var text = "Hello, World!"
-
-    public static func main() {
-        print(bulk_builder_test_xcode().text)
-    }
-}
---
+++ bulk-builder-test/bulk_builder_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  bulk_builder_testApp.swift
+//  bulk-builder-test
+//
+//  Created by Koen Vlaswinkel on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct bulk_builder_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
